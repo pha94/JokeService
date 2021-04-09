27f19818f69adf5631445b76ea375df6141d5bdd
Patrick Andersen½ <eaaphand@students.eaaa.dk>
2021-04-09 14:26:47 +0200

    config file

---
+++ JS/config.js
@@ -0,0 +1,7 @@
+const config = {
+  databaseURI:
+    "mongodb+srv://dbpzh:<password>@jokeservice.gfzx8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+localPort: 8080
+};
+
+module.exports = config;
