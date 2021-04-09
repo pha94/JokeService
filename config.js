8b6cf7867c70584a16f4534184564f6ea2f886e9
Patrick Andersen½ <eaaphand@students.eaaa.dk>
2021-04-09 14:57:30 +0200

    edit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   databaseURI:
     "mongodb+srv://dbpzh:dbpzh123@jokeservice.gfzx8.mongodb.net/JokeService?retryWrites=true&w=majority",
-localPort: 8080
+  localPort: 8080,
 };
 
 module.exports = config;
