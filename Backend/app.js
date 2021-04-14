3f331775ec71deb2cb589bc6f4e07fbdfdd448cb
Patrick Andersen½ <eaaphand@students.eaaa.dk>
2021-04-14 11:44:26 +0200

    app ændret

--- Backend/app.js
+++ Backend/app.js
@@ -1,40 +1,38 @@
-const controller = require('../Controller/controller');
-const express = require('express');
+const controller = require("../Controller/controller");
+const express = require("express");
 const app = express();
-const config = require('./config');
+const config = require("./config");
 
-app.use(express.static(__dirname + '/FrontEnd'));
+app.use(express.static(__dirname + "/FrontEnd"));
 app.use(express.json());
-app.use('/jokes', require('/jokes'));
+app.use("/jokes", require("/jokes"));
 
 const port = process.env.PORT || config.localPort; // Heroku
 app.listen(port);
-console.log('Listening on port ' + port + ' ...');
-
-
+console.log("Listening on port " + port + " ...");
 
 app.get("/", async (request, response) => {
-    try {
-        let jokes = await controller.getJokes();
-        response.send(jokes);
-    } catch (e) {
-        sendStatus(e, response);
-    }
+  try {
+    let jokes = await controller.getJokes();
+    response.send(jokes);
+  } catch (e) {
+    sendStatus(e, response);
+  }
 });
 
 app.post("/", async (request, response) => {
-    try {
-        let { name, setup, punchline } = request.body;
-        await controller.createJoke(name, setup, punchline);
-        response.send({ message: "Joke saved!" });
-    } catch (e) {
-        sendStatus(e, response);
-    }
+  try {
+    let { name, setup, punchline } = request.body;
+    await controller.createJoke(name, setup, punchline);
+    response.send({ message: "Joke saved!" });
+  } catch (e) {
+    sendStatus(e, response);
+  }
 });
 
 function sendStatus(e, response) {
-    console.error("Exception: " + e);
-    if (e.stack) console.error(e.stack);
-    response.status(500).send(e);
-    
-    module.exports = app; // test
+  console.error("Exception: " + e);
+  if (e.stack) console.error(e.stack);
+  response.status(500).send(e);
+}
+module.exports = app; // test
