5c8c293018c2446a3689d5d8970c02b5a5a393f1
Patrick Andersen½ <eaaphand@students.eaaa.dk>
2021-04-14 11:20:02 +0200

    new files ex. procfile

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/FrontEnd'));
 app.use(express.json());
-app.use('/company', require('./routes/company'));
-app.use('/employee', require('./routes/employee'));
+app.use('/jokes', require('/jokes'));
 
 const port = process.env.PORT || config.localPort; // Heroku
 app.listen(port);
