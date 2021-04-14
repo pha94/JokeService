e25410d58f5fa28458877602f4065ba8fcffaa59
Patrick Andersen½ <eaaphand@students.eaaa.dk>
2021-04-14 10:19:31 +0200

    new update clean install mustache, express, mongoose

--- JS/index.js
+++ JS/index.js
@@ -2,11 +2,12 @@ const mongooseDB = require("mongoose");
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const jokes = mongooseDB.model("JokeServiceDB.JokeService", jokeServiceSchema);
 
 mongooseDB.connect("mongodb://localhost:27017/JokeServiceDB.JokeService", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
 });
 
 console.log("hej");
@@ -23,26 +24,24 @@ app.listen(PORT, () => {
   console.log(`app is listening to PORT ${PORT}`);
 });
 
-async function createJoke(name, setup, punchline) {
-  return await jokes.create({ name, setup, punchline });
-}
-
-Joke.create = (name, setup, punchline) => {
-  Joke.name = name;
-  Joke.setup = setup;
-  Joke.punchline = punchline;
-};
-
 const jokeServiceSchema = new mongooseDB.Schema({
   name: String,
   setup: String,
   punchline: String,
 });
 
-document.getElementById("uploadBtn").addEventListener("click", () => {
+const jokes = mongooseDB.model("JokeServiceDB.JokeService", jokeServiceSchema);
+
+async function createJoke(name, setup, punchline) {
+  return await jokes.create({ name, setup, punchline });
+}
+
+document.getElementById("uploadBtn").addEventListener("click", async () => {
   let name = document.getElementById("name").value;
   let setup = document.getElementById("setup").value;
   let punchline = document.getElementById("punchline").value;
+
+  await createJoke(name, setup, punchline);
   console.log("button");
   if (!name.equals("") && !setup.equals("") && !punchline.equals("")) {
     jokes.create(name, setup, punchline);
