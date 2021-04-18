6dbc2ecc5474d1c4dbcbf04c6f23c816e9c25e88
Patrick Andersen½ <eaaphand@students.eaaa.dk>
2021-04-18 09:56:00 +0200

    server

--- Backend/app.js
+++ Backend/app.js
@@ -3,13 +3,41 @@ import { express, static, json } from "express";
 import { localPort } from "/config.js";
 const app = express();
 
+
+const http = require('http');
+const fs = require("fs");
+const port = 3000;
+
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("../Frontend/index.html", function (error, data) {
+    if (error) {
+      res.writehead(404)
+      res.write("Error: File not found");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+server.listen(port, function (error) {
+  if (error) {
+    console.log("Something went wrong", error); 
+  } else {
+    console.log("Server is listening on port " + port);
+  }
+});
+
+
+
 app.use(static(__dirname + "/Frontend"));
 app.use(json());
 app.use("/jokes", require("/jokes"));
 
-const port = process.env.PORT || localPort; // Heroku
-app.listen(port);
-console.log("Listening on port " + port + " ...");
+// const port = process.env.PORT || localPort; // Heroku
+// app.listen(port);
+// console.log("Listening on port " + port + " ...");
 
 app.get("/", async (request, response) => {
   try {
