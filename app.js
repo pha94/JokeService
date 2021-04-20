bf5128b242c14cde29a150e69fb24a4f25e75b7e
Patrick Andersen½ <eaaphand@students.eaaa.dk>
2021-04-20 14:07:12 +0200

    klkl

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { createJoke } from "./Controller/controller.js";
+import { createJoke } from "./controller/controller.js";
 import express from "express";
 import config from "./config.js"; // MongoDB
 const app = express();
